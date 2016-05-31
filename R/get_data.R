455236c974c597415a73505ca876fb8e2705b4a8
Zachary T Cross <zachary.t.cross@gmail.com>
2016-05-31 16:56:56 -0400

    Updated get_bls_data() to support multiple series ids
    Can now request multiple series ids from get_bls_data().  Requires a
    BLS-provided registration code, just like get_bls_info().

--- R/get_data.R
+++ R/get_data.R
@@ -6,26 +6,45 @@ library(httr)
 #' @return data.frame with data for the requested series id.
 #' @export
 #' @examples
-#' get_bls_data()
-#' get_bls_data('LAUCN040010000000005')
-get_bls_data <- function(series_id='LAUCN040010000000005') {
-    url <- paste0('http://api.bls.gov/publicAPI/v2/timeseries/data/', series_id)
-    url <- URLencode(url)
-    response <- httr::GET(url, add_headers('content-type'='application/json'))
+#' get_bls_data(registration_key='BLS-provided key')
+#' get_bls_data('LAUCN040010000000005', registration_key='BLS-provided key')
+get_bls_data <- function(series_ids='LAUCN040010000000005', registration_key=NA) {
+    url <- 'http://api.bls.gov/publicAPI/v2/timeseries/data/'
 
-    c <- content(response)
-
-    if (length(c$message) > 0){
-        stop(c$message)
-    } else {
-        series_id <- c$Results$series[[1]]$seriesID
-        data <- c$Results$series[[1]]$data
-
-        df <- data.frame(year = as.numeric(sapply(data, '[[', 'year')),
-                         period = sapply(data, '[[', 'period'),
-                         periodName = sapply(data, '[[', 'periodName'),
-                         value = sapply(data, '[[', 'value'))
-        df <- df[order(df$year, df$period), ]
-        return(df)
+    # Convert `series_ids` into a list
+    if (length(series_ids) < 2) {
+        series_ids <- list(series_ids)
     }
+
+    # Check registration_key
+    if (class(registration_key) == 'logical') {
+        stop('argument registration_key required')
+    }
+
+    body_list = list(seriesid = series_ids, registrationKey=registration_key)
+    response = httr::POST(url, body=rjson::toJSON(body_list), config=add_headers('content-type'='application/json'))
+
+    c <- httr::content(response)
+
+    if (length(c$message) > 0) {
+        stop(c$message)
+    }
+    final_df <- NA
+    first = TRUE
+    for (current in c$Results$series) {
+        cur_series_id <- current$seriesID
+        df <- data.frame(year=as.numeric(sapply(current$data, '[[', 'year')),
+                         period=sapply(current$data, '[[', 'period'),
+                         periodName=sapply(current$data, '[[', 'periodName'),
+                         value=as.numeric(sapply(current$data, '[[', 'value')))
+
+        names(df) <- c('year', 'period', 'periodName', cur_series_id)
+        if (first) {
+            final_df <- df
+        } else{
+            final_df <- merge(final_df, df, by=c('year', 'period', 'periodName'), all=TRUE, sort=TRUE)
+        }
+        first = FALSE
+    }
+    return(final_df)
 }
